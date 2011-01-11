97f6753e492a7eee5b9d3ef0d23445625c5a3aed
Keith Pitt <me@keithpitt.com>
2011-01-12 07:31:36 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxPagination::Application.config.secret_token = '35157cfe1f7daf995ae8f3fd93f7342bf19a89e05b07e9ab1416a6272a33b43c1d3cebfadabc5693231f120694d31cdf7b74c42244c13dd03b5622df7e93052b'
