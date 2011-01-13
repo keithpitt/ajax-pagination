999290362daf710bad3c2fbd1cb5bfe9a8a94f00
Keith Pitt <me@keithpitt.com>
2011-01-13 08:50:12 +0800

    Improved paginator

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -1,35 +1,54 @@
+
+$('.ajax-results').live('submit', function() {
+  var action = $(this).attr('action');
+  var params = $(this).serialize();
+  if(Paginator.hasPushSupport())
+    window.location = action + params;
+  else
+    window.location = action + '#/?' + params;
+  return false;
+})
+
 var Paginator = {
 
   init: function(searchPath) {
-      this.setup(searchPath);
+    if(!this.hasPushSupport() && !window.location.search) {
+      this.path = searchPath || window.location.path;
+      this.setup();
+    }
   },
 
-  setup: function(searchPath) {
-
-    $('#results').html('');
+  setup: function() {
 
     $.address.state('/');
 
     $.address.change(function(event) {
+      $('#results').html('Loading...');
       $.ajax({
-        url: searchPath,
+        url: window.location.path,
         data: event.parameters,
         dataType: 'script',
         type: 'GET',
         success: function(html) {
           $('#results').html(html);
         }
-      })
+      });
     });
-
 
     $(document).ready(function() {
       $('.pagination a').live('click', function() {
-        $.address.value($(this).attr('href'));
+        var href = $(this).attr('href');
+        var path = Paginator.hasPushSupport() ? href : href.replace(Paginator.path, '');
+
+        $.address.value(path);
         return false;
       })
     })
 
+  },
+
+  hasPushSupport: function() {
+    return history.pushState ? true : false;
   }
 
 }
