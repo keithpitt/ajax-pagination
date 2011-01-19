81459cfe8d45a02087377eac7bd607b2211c27f0
Keith Pitt <me@keithpitt.com>
2011-01-19 22:26:21 +0800

    Few final tweaks

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,12 +4,7 @@ class SearchController < ApplicationController
     @query = params[:q]
     @results = Game.search(@query).paginate :page => params[:page], :per_page => 10
 
-    respond_to do |wants|
-      wants.html
-      wants.js do
-        render :partial => 'results', :locals => { :results => @results }
-      end
-    end
+    render :layout => false if request.xhr?
   end
 
 end
